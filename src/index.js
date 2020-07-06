12c451da7b6cb1810da0cdb303325d9a7ce45234
https://learn.co/christa-gammage <crgammage@gmail.com>
2020-07-06 17:28:48 -0400

    cocktail isnt working

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-const COCKTAIL_URL = "http://localhost:3000/cocktails"
+const COCKTAIL_URL = "http://localhost:3000/cocktail_ingredients"
 
 fetch(COCKTAIL_URL)
 .then(response => response.json())
-.then(cocktails => {
-    cocktails.forEach(cocktail => {
-        renderCocktail(cocktail)
-    })
-
+.then(data => {
+  data.map(cocktailIngredient => {
+    console.log(cocktailIngredient.cocktail)
+    }) 
 })
+  // cocktails => {
+    // cocktails.forEach(cocktail => {
+    //   console.log(cocktail)
+    //     renderCocktail(cocktail)
+    //     renderIngredients(cocktail)
+    //     })
+    
 
 function renderCocktail(cocktail){
     let cards = document.querySelector('.cards')
@@ -16,11 +22,11 @@ function renderCocktail(cocktail){
     cocktailCard.innerHTML = `
     <h3 class="card-header">${cocktail.name}</h3>
     <div class="card-body">
-      <h5 class="card-title">Drink Bio</h5>
+      <h5 class="card-title">${cocktail.bio}</h5>
     </div>
-    <img class="drink-img" style="height: 200px; width: 50%; display: block;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22318%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20318%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_158bd1d28ef%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A16pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_158bd1d28ef%22%3E%3Crect%20width%3D%22318%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22129.359375%22%20y%3D%2297.35%22%3EImage%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image">
+    <img class="drink-img" style="height: 200px; width: 50%; display: block;" src=${cocktail.image}>
     <div class="card-body">
-      <p class="card-text">Ingredients.</p>
+      <p class="card-text">Ingredients</p>
     </div>
     <ul class="list-group list-group-flush">
       <li class="list-group-item">1</li>
@@ -48,6 +54,18 @@ function renderCocktail(cocktail){
     `
     cards.append(cocktailCard)
 }
+
+function renderIngredients(cocktail) {
+  cocktail.ingredients.forEach(ingredient => {
+    let ingredientUl = document.querySelector("body > main > div.cards > div:nth-child(1) > ul")
+    let ingredientLi = document.createElement("li")
+    ingredientLi.className = "list-group-item"
+    ingredientLi.innerText = `${ingredient.name} - ${ingredient.amount}`
+    ingredientUl.append(ingredientLi)
+  })
+}
+
+
 
 
 
